7b240c853fb38f526d0d5fac3e2e3d75f068641e
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-07-10 14:45:46 +0200

    Modify the script to also support Sticky headers.

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
+/* ************************************************************************
+    Sticky Definitions
+************************************************************************ */
 $(function(){
-
-    var footer = $("footer");
-    var sticky = footer.find('div.visibleSticky');
-    var push = $("#content div.push");
-    var content = $("#content");
-    var visibleBottom = sticky.outerHeight() || 50;
+   
+    /* General Function */
+    var stickyFunction = function(definition){
     
-    var calcPosition = function() {
-       var height = footer.outerHeight();
-       push.height(height);
-       content.css("margin-bottom", -height);
-       footer.css('bottom', -(height - visibleBottom));
-    }
-   
-   var fixPosition = function() {
-       if ( push.offset().top >= $(window).scrollTop() + $(window).height() - visibleBottom ) {
-           footer.addClass("sticky");
-       } else {
-           footer.removeClass("sticky");
-       }
-   };
-   
-   $(window).on( "load resize", calcPosition );
-   $(window).on( "load resize scroll", fixPosition );
+        var element = $(definition.root);
+        if ( !element.length > 0 ) {
+            return;
+        }
+        
+        var sticky = element.find("div.visibleSticky");
+        var visibleArea = sticky.outerHeight() || 50;
+        
+        var content = $("#content");
+        var push = content.find('div.push-' + definition.root);
+        
+        var calcPosition = function() {
+        var height = element.outerHeight();
+            push.height(height);
+            content.css('margin-' + definition.stickTo, -height);
+            element.css(definition.stickTo, -(height - visibleArea));
+        }
+        
+        var fixPosition = function() {
+            if ( definition.stickyFunction(push, visibleArea) ) {
+                element.addClass("sticky");
+            } else {
+                element.removeClass("sticky");
+            }
+        };
+        
+        $(window).on( "load resize", calcPosition );
+        $(window).on( "load resize scroll", fixPosition );
+    };
+    
+    /* HEADER */
+    var headerDef = {
+        root : 'header',
+        stickTo : 'top',
+        stickyFunction : function(pushElement, visibleArea) {
+            return $(window).scrollTop() >= pushElement.height() - visibleArea;
+        }
+    };
+    
+    /* FOOTER */
+    var footerDef = {
+        root : 'footer',
+        stickTo : 'bottom',
+        stickyFunction : function(pushElement, visibleArea) {
+            return pushElement.offset().top >= $(window).scrollTop() + $(window).height() - visibleArea;
+        }
+    };
+    
+    /* INIT */
+   stickyFunction(headerDef);
+   stickyFunction(footerDef);
 });
